7c920d8494a554c7e75f3ec693e95abf7db52785
healer1064 <healer1064@outlook.com>
2021-05-21 01:58:11 -0400

    Getting started with Redux Saga. Initializing Saga with an empty root saga

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,14 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import createSagaMiddle from 'redux-saga'
+
 
 // ROOT REDUCER
 import rootReducer from './rootReducer'
 
+// ROOT SAGA
+import rootSaga from './rootSaga'
+
 //REDUX MIDDLEWARES
-export const middlewares = [thunk, logger]
+// ...(instance of Saga to add to middlewares array)
+       const sagaMiddleware = createSagaMiddle()
+export const middlewares = [thunk, sagaMiddleware, logger]
 
 
 //THE STORE
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+sagaMiddleware.run(rootSaga)
+
+
 export default store
