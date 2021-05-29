29b8528272a80de2a32b5e7e6eb14d8997dd3269
healer1064 <healer1064@outlook.com>
2021-05-28 23:31:03 -0400

    conditional rendering admin tool bar, and admin route based on of user roles array contains admin string.

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSelector } from 'react-redux'
 
 const StyledDiv = styled.div``
 
+const mapState = ({ user }) => ({
+    currentUser: user.currentUser
+})
+
 export default function Admin(){
+    const { currentUser } = useSelector(mapState)
     return (
         <StyledDiv>
+            <h6>Hi there, {currentUser.displayName}!</h6>
             <h1>My Admin</h1>
         </StyledDiv>    
     )
