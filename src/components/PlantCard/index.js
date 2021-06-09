f02196415dc13bb566f70b7576f9f14424756c09
healer1064 <healer1064@outlook.com>
2021-06-09 01:02:43 -0400

    PlantCards display individual information. Fixed jumping UI bug where reudx store for fetched plant was hold the previous plant rather than being empty.

--- src/components/PlantCard/index.js
+++ src/components/PlantCard/index.js
@@ -1,12 +1,96 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
-const StyledDiv = styled.div``
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAPlantStart, setAPlant } from './../../redux/plants/plants.actions'
+
+import Button from '../forms/Button'
+
+const StyledDiv = styled.div`
+    max-width: 80.0rem;
+    margin: 0 auto 10.0rem;
+
+    .hero {
+        display: block;
+        width: 100%;
+        margin: 2rem auto;
+
+        img {
+            display: block;
+            width: 100%;
+            margin: 0;
+
+        }
+    }
+
+    .plant_details {
+        ul, ul li{
+            margin: 0;
+            padding: 0;
+        }
+
+        ul li {
+            list-style-type: none;
+            margin: 0 auto 1rem;
+
+            h1 {
+                margin: 0;
+            }
+        }
+    }
+
+    .add_to_cart {
+        max-width: 20.0rem;
+    }
+
+`
+
+
+const mapState = ({ plants }) => ({
+    aPlant: plants.aPlant
+})
 
 export default function PlantCard({}){
+    const dispatch = useDispatch()
+    const { plantID } = useParams()
+    const { aPlant } = useSelector(mapState)
+
+    const { plantName, plantThumbnail, plantPrice } = aPlant
+
+    useEffect(() => {
+        dispatch(fetchAPlantStart(plantID))
+
+        return () => {
+            dispatch(setAPlant({}))
+        }
+    },[])
+
+    const configAddToCartBtn = {
+        type: 'button'
+    }
     return (
         <StyledDiv>
-            <h1>test</h1>
+            <div className="hero">
+                <img src={plantThumbnail}/>
+            </div>
+            <div className="plant_details">
+                <ul>
+                    <li>
+                        <h1>{plantName}</h1>
+                    </li>
+                    <li>
+                        <span>${plantPrice}</span>
+                    </li>
+                    <li>
+                        <div className="add_to_cart">
+                            <Button {...configAddToCartBtn}>
+                                Add to Cart
+                            </Button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </StyledDiv>
     )
 }
